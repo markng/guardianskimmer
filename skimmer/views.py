93daaf3ea935b8574f725cdf48af1a58acbfe55c
Mark Ng <mark@markng.co.uk>
2009-03-11 17:57:39 +0000

    add date from to skimmer view

--- skimmer/views.py
+++ skimmer/views.py
@@ -2,12 +2,14 @@ from openplatform.guardianapi import Client
 from django.shortcuts import render_to_response
 from django.utils.http import urlencode
 from django.conf import settings
+import datetime
 
 def skim(request, **args):
   """skim view"""
+  time = datetime.datetime.now() - datetime.timedelta(days=1)
   client = Client(settings.GUARDIAN_API_KEY)
   if args.has_key('search'):
-    results = client.search(q = '/' + args['search'])
+    results = client.search(q = '/' + args['search'], after = time.strftime('%Y%m%d'))
   else:
-    results = client.search(q = '')
+    results = client.search(q = '', after = time.strftime('%Y%m%d'))
   return render_to_response('skimmer/skim.html', {'articles' : results})
