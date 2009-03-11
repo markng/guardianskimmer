716f6c487ae724410f7b8ca08de5b0a7086c3805
Mark Ng <mark@markng.co.uk>
2009-03-11 17:04:48 +0000

    add stylesheet urls for static serve

--- urls.py
+++ urls.py
@@ -1,5 +1,13 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from os import path as os_path
+
 
 urlpatterns = patterns('',
+  ( r'^stylesheets/(.*)$', 
+    'django.views.static.serve', 
+    {'document_root': os_path.join(settings.PROJECT_PATH, 'media/stylesheets')}, 
+    'stylesheets'
+  ),
   (r'^', include('skimmer.urls')),
 )
