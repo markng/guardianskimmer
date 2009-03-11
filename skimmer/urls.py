b1ede4b12afe790913cc979075be087ba9e1c512
Mark Ng <mark@markng.co.uk>
2009-03-11 14:59:38 +0000

    start the skimmer

--- skimmer/urls.py
+++ skimmer/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-  (r'^', include('skimmer.urls')),
+  (r'^$', 'skimmer.views.skim', {}, "home"),
 )
--- skimmer/views.py
+++ skimmer/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from openplatform.guardianapi import Client
+from django.shortcuts import render_to_response
+from django.conf import settings
+
+def skim(request):
+  """skim view"""
+  client = Client(settings.GUARDIAN_API_KEY)
+  results = client.search(q = 'ocelots')
+  return render_to_response('skimmer/skim.html', {'articles' : results})
