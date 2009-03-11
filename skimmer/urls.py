578fb0ead63e2355b5215e6fbc6966b3225955a1
Mark Ng <mark@markng.co.uk>
2009-03-11 16:40:06 +0000

    add a nav, make it start to work (we're not working for keywords with / in them yet)

--- skimmer/urls.py
+++ skimmer/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
   (r'^$', 'skimmer.views.skim', {}, "home"),
+  (r'^k/(?P<search>.*)$', 'skimmer.views.skim', {'type' : 'keyword'}, "skim"),
 )
--- skimmer/views.py
+++ skimmer/views.py
@@ -1,9 +1,13 @@
 from openplatform.guardianapi import Client
 from django.shortcuts import render_to_response
+from django.utils.http import urlencode
 from django.conf import settings
 
-def skim(request):
+def skim(request, **args):
   """skim view"""
   client = Client(settings.GUARDIAN_API_KEY)
-  results = client.search(q = '/technology')
+  if args.has_key('search'):
+    results = client.search(q = '/' + args['search'])
+  else:
+    results = client.search(q = '')
   return render_to_response('skimmer/skim.html', {'articles' : results})
